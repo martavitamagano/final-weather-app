26e3ee7c02b9cab08749bc66308f24df6626c730
martavitamagano <martinha.magano@gmail.com>
2024-02-17 05:00:37 +0000

    some updates plus js

--- weather.js
+++ weather.js
@@ -1,9 +1,37 @@
+function weatherInfo(response){
+    console.log(response);
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let weatherInfoElement = document.querySelector("#weatherInfo");
+    let humidityElement = document.querySelector("#humidityPercentage");
+    let windElement = document.querySelector("#windKm");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    weatherInfoElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windElement.innerHTML = response.data.wind.speed;
+}
+
+
+
+function searchCity(city){
+let apiKey = "4foba0te46425f633e30d965f9ae409d";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(weatherInfo);
+}
+
+
+
+
 function handleSearch(event){
+
     event.preventDefault();
     let searchInput = document.querySelector("#searchBar");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
 
+searchCity(searchInput.value);
 }
 
 
