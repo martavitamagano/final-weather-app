339f2e319976b709542db8fd72037061f8db0586
martavitamagano <martinha.magano@gmail.com>
2024-02-20 01:10:38 +0000

    time and date settled

--- weather.js
+++ weather.js
@@ -6,15 +6,36 @@ function weatherInfo(response){
     let weatherInfoElement = document.querySelector("#weatherInfo");
     let humidityElement = document.querySelector("#humidityPercentage");
     let windElement = document.querySelector("#windKm");
+     let timeElement = document.querySelector("#dateTime");
+     let date = new Date(response.data.time * 1000);
+
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = Math.round(temperature);
     weatherInfoElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = response.data.wind.speed;
 }
 
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    let dayNumber = date.getDate();
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month = months[date.getMonth()];
 
+    if(minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    if(hours < 10){
+        hours = `0${hours}`;
+    }
+
+    return `${day} , ${dayNumber} ${month} , ${hours}:${minutes}`;
+}
 
 function searchCity(city){
 let apiKey = "4foba0te46425f633e30d965f9ae409d";
@@ -33,6 +54,11 @@ function handleSearch(event){
 
 searchCity(searchInput.value);
 }
+
+
+  
+
+
 
 
 let searchFormElement = document.querySelector("#searchForm");
