9deefcb148feb0c435a19a1b0d2481e11ffe3961
martavitamagano <martinha.magano@gmail.com>
2024-03-08 00:56:37 +0000

    add forecast.

--- weather.js
+++ weather.js
@@ -58,6 +58,32 @@ function handleSearch(event){
 searchCity(searchInput.value);
 }
 
+function displayForecast(){
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+days.forEach(function(day){
+  forecastHtml =
+  forecastHtml +
+
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15°</strong>
+          </div>
+          <div class="weather-forecast-temperature">9°</div>
+        </div>
+      </div>
+    `;
+
+})
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+  
+}
 
   
 
@@ -66,3 +92,5 @@ searchCity(searchInput.value);
 
 let searchFormElement = document.querySelector("#searchForm");
 searchFormElement.addEventListener("submit", handleSearch);
+
+displayForecast();
